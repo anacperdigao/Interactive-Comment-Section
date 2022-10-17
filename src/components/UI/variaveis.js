f0eb52886a4dbe2d51f0012c51533c3eb493b003
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-17 17:31:08 -0300

    adicionando variaveis de cor e GlobalStyle

---
+++ src/components/UI/variaveis.js
@@ -0,0 +1,14 @@
+// Colors
+
+// Primary
+export const moderateBlue = "hsl(238, 40%, 52%)";
+export const softRed = "hsl(358, 79%, 66%)";
+export const lightGrayishBlue = "hsl(239, 57%, 85%)";
+export const paleRed = "hsl(357, 100%, 86%)";
+
+// Neutral
+export const darkBlue = "hsl(212, 24%, 26%)";
+export const grayishBlue = "hsl(211, 10%, 45%)";
+export const lightGray = "hsl(223, 19%, 93%)";
+export const veryLightGray = "hsl(228, 33%, 97%)";
+export const white = "hsl(0, 0%, 100%)";
